4173425d6f720711632f8af13da9493bcfc13a04
davidlechuga <47370612+davidlechuga@users.noreply.github.com>
2020-04-09 22:23:55 -0500

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
 
-const mongoURI = 'mongodb+srv://Hacks:david2001@cluster0-arnoh.mongodb.net/test'
+const mongoURI = ''
 
 mongoose.connect(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
     .then ( () => console.log("MongoDB Connected"))
